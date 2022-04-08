063c1b911e1d545a0f5b28930ac4c6eac635aaa1
dhyces <pokmon987@gmail.com>
2022-04-08 15:14:14 -0400

    Add client side init

---
+++ src/main/java/dhyces/echoknight/EchoKnightClient.java
@@ -0,0 +1,13 @@
+package dhyces.echoknight;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class EchoKnightClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
