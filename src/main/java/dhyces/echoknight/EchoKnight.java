decb8c4f3aea2d2db7bf1e226f9869c4c49d7fe1
dhyces <pokmon987@gmail.com>
2022-04-09 15:22:34 -0400

    Reordering

--- src/main/java/dhyces/echoknight/EchoKnight.java
+++ src/main/java/dhyces/echoknight/EchoKnight.java
@@ -3,11 +3,10 @@ package dhyces.echoknight;
 import dhyces.echoknight.entity.EchoEntity;
 import dhyces.echoknight.item.EchoApple;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -18,12 +17,11 @@ public class EchoKnight implements ModInitializer {
         return new Identifier(MOD_ID + ":" + id);
     }
 
+    public static final Item ECHO_APPLE_ITEM = Registry.register(Registry.ITEM, id("echoed_apple"), new EchoApple(new Item.Settings().food(new FoodComponent.Builder().hunger(2).saturationModifier(3).build()).group(ItemGroup.FOOD)));
+
+    public static final EntityType<EchoEntity> ECHO_ENTITY_TYPE = Registry.register(Registry.ENTITY_TYPE, id("echo_entity"), EchoEntity.createType().build());
+
     @Override
     public void onInitialize() {
-        FabricDefaultAttributeRegistry.register(ECHO_ENTITY_TYPE, EchoEntity.createAttributes());
     }
-
-    public static final Item ECHO_APPLE_ITEM = Registry.register(Registry.ITEM, id("echoed_apple"), new EchoApple(new Item.Settings().food(new FoodComponent.Builder().hunger(2).saturationModifier(3).build())));
-
-    public static final EntityType<EchoEntity> ECHO_ENTITY_TYPE = Registry.register(Registry.ENTITY_TYPE, id("echo_entity"), FabricEntityTypeBuilder.createLiving().entityFactory(EchoEntity::new).trackRangeBlocks(30).build());
 }
